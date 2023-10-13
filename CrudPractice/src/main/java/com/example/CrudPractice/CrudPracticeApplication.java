6b27689bfd685d929bd26ea4fa00e80b9e051094
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-13 19:39:36 -0400

    @ManyToMany 1. Configure Student class for ManyToMany, JoinTable, JoinColumn, InverseJoinColumn 2. Configure Course class for ManyToMany, JoinTable, JoinColumn, InverseJoinColumn 3. Add methods to Main App

--- CrudPractice/src/main/java/com/example/CrudPractice/CrudPracticeApplication.java
+++ CrudPractice/src/main/java/com/example/CrudPractice/CrudPracticeApplication.java
@@ -1,10 +1,7 @@
 package com.example.CrudPractice;
 
 import com.example.CrudPractice.dao.AppDAO;
-import com.example.CrudPractice.entity.Course;
-import com.example.CrudPractice.entity.Instructor;
-import com.example.CrudPractice.entity.InstructorDetail;
-import com.example.CrudPractice.entity.Review;
+import com.example.CrudPractice.entity.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,13 +21,32 @@ public class CrudPracticeApplication {
 
 		return runner -> {
 
-			// createCourseAndReviews(appDAO);
+			createCourseAndStudents(appDAO);
 
-			// retrieveCourseAndReviews(appDAO);
-
-			deleteCourseAndReviews(appDAO);
 
 		};
+	}
+
+	private void createCourseAndStudents(AppDAO appDAO) {
+
+		// create a course
+		Course tempCourse = new Course("Pacman - How To Score One Million Points");
+
+		// create the students
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Stephanie", "Lion", "stephanie@luv2code.com");
+
+		// add students to the course
+		tempCourse.addStudent(tempStudent1);
+		tempCourse.addStudent(tempStudent2);
+
+		// save the course and associated students
+		System.out.println("Saving the course: " + tempCourse);
+		System.out.println("associated students: " + tempCourse.getStudents());
+
+		appDAO.save(tempCourse);
+
+		System.out.println("Done!");
 	}
 
 	private void deleteCourseAndReviews(AppDAO appDAO) {
--- CrudPractice/src/main/java/com/example/CrudPractice/entity/Course.java
+++ CrudPractice/src/main/java/com/example/CrudPractice/entity/Course.java
@@ -17,13 +17,22 @@ public class Course {
     @Column(name="title")
     private String title;
 
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                        CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name="instructor_id")
     private Instructor instructor;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="course_id")
     private List<Review> reviews;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                        CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
 
     public Course() {
     }
@@ -73,6 +82,25 @@ public class Course {
         }
 
         reviews.add(theReview);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    // add a convenience method
+
+    public void addStudent(Student theStudent) {
+
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+
+        students.add(theStudent);
     }
 
     @Override

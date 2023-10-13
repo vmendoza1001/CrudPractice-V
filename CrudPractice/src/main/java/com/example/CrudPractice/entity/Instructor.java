6ecf69e2ca84e3dfe5931f2ee5cd062832cc194a
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-12 20:25:14 -0400

    Mappings 1. Create Many-to-One relationship for course to instructor 2. Create One-to-Many relationship for Instructor to course 3. Add method for bi-directional relationship

--- CrudPractice/src/main/java/com/example/CrudPractice/entity/Instructor.java
+++ CrudPractice/src/main/java/com/example/CrudPractice/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.example.CrudPractice.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="instructor")
 
@@ -23,6 +26,10 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor",
+                cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH} )
+    private List<Course> courses;
 
     public Instructor() {
     }
@@ -82,5 +89,26 @@ public class Instructor {
                 ", email='" + email + '\'' +
                 ", instructorDetail=" + instructorDetail +
                 '}';
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // add convenience methods for bi-directional relationship
+
+    public void add(Course tempCourse) {
+
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
     }
 }
